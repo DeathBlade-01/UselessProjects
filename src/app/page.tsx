2fca35d0dac7b8387cfc383b4eb1e9d3e2fb6cad
DeathBlade-01 <harikrishnanjhkhari@gmail.com>
2024-11-02 22:09:56 +0530

    ALPHA PHASE1

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import TypingChallenge from './components/TypingChallenge'
+
+export default function Home() {
+  return (
+    <main className="container mx-auto p-4">
+      <TypingChallenge />
+    </main>
+  )
+}
